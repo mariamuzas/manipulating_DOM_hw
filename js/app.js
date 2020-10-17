c8404f30263dc2b4dcd916b9f7f32551aabbca0e
Your Name <mmuzasantuna@gmail.com>
2020-10-17 13:36:05 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript loaded');
+
+   const form = document.querySelector('#new-hit-form');
+   form.addEventListener('submit', handleForm);
+
+   const deleteAll = document.querySelector('#delete-button');
+   deleteAll.addEventListener('click', handleDeleteButton);
+});
+
+const handleForm = (event) => {
+    event.preventDefault();
+
+    const newHit = document.createElement('li');
+
+    const musicList = document.querySelector('#hits-list');
+    newHit.textContent = `${this.song.value}, ${this.artist.value}, ${this.album.value}, ${this.year.value}`;
+
+    event.target.reset();
+
+    musicList.appendChild(newHit);
+};
+
+const handleDeleteButton = () => {
+    const musicList = document.querySelector('#hits-list');
+    while(musicList.firstChild) {
+        musicList.firstChild.remove()
+    };
+};
